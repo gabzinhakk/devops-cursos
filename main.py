aae29af9d99ca94977da08ecfafb9d617f5aff73
Gabriela Soares <gabzinhakkj@gmail.com>
2026-04-18 13:11:51 -0300

    Lista de cursos
    Adicionei uma lista de cursos de segurança do trabalho

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+cursos = [
+    {"id": 1, "nome": "NR-35 Trabalho em Altura"},
+    {"id": 2, "nome": "NR-10 Segurança em Eletricidade"}
+]
+
 @app.get("/")
 async def root():
     return {"message": "API de Treinamentos"}
