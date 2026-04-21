84018bb84d5c3a9f65696776503536d876649c20
Gabriela Soares <gabzinhakkj@gmail.com>
2026-04-20 22:39:43 -0300

    CI ajuste

--- test_main.py
+++ test_main.py
@@ -6,3 +6,14 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
+    assert response.json()["status"] == "online"
+
+def test_listar_cursos():
+    response = client.get("/cursos")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_buscar_curso():
+    response = client.get("/cursos/1")
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
